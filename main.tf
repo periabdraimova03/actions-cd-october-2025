544f3a2e8d0b0cad061e26843ba4f1663b2b6011
periabdraimova03 <peri.abdraimova@gmail.com>
2026-04-28 00:55:02 +0000

    tf file added

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+resource "helm_release" "flask" {
+  name       = "flask-app"
+  chart      = "./flask-app"
+  version    = "0.1.0"
+  force_update = true
+
+  set = [
+    {
+      name  = "service.type"
+      value = "ClusterIP"
+    }
+  ]
+}
